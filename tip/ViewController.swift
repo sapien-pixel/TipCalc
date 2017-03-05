49b1a462d61d57eb7e2723d4768f6d6b376afa22
Dhruv Kalaria <dhruvkalaria@gmail.com>
2017-03-05 01:34:56 -0800

    Added Implementation in View Controller

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -16,10 +16,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var billIncludingTip: UILabel!
     @IBOutlet weak var numberOfPeople: UILabel!
     @IBOutlet weak var splitAmount: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var splitSlider: UISlider!
+    
+    var billStr = "0.00"
+    var tipStr = "0.00"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
+        tipPercent.text = "0%"
+        numberOfPeople.text = "1"
+        billAmount.text="0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,9 +41,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tipPercentChanged(_ sender: Any) {
+        tipPercent.text = String(format: "%.0f", tipSlider.value) + "%"
+        tipAmount.text = String(format: "%.2f",Float(billStr)! * 0.01 * Float(tipStr)!)
+        billAmoutChanged(nil)
     }
     
     @IBAction func splitNumberChanged(_ sender: Any) {
+        numberOfPeople.text = String(format: "%.0f",splitSlider.value)
+        splitAmount.text = String(format: "%.2f",Float(billIncludingTip.text!)!/Float(numberOfPeople.text!)!)
     }
+    
+    @IBAction func billAmoutChanged(_ sender: UITextField?) {
+        billStr = (billAmount.text?.replacingOccurrences(of: "$", with: ""))!
+        tipStr = (tipPercent.text?.replacingOccurrences(of: "%", with: ""))!
+        billIncludingTip.text = String(format: "%.2f",Float(billStr)! * (1 + 0.01 * Float(tipStr)!))
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
 }
 
